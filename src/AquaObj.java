f2ec345460c95aaba253d719a60940331602a9fa
Vadym Skoryk <yopt322@gmail.com>
2019-06-11 18:06:22 +0300

    Write some methods and refactor

--- src/AquaObj.java
+++ src/AquaObj.java
@@ -5,13 +5,11 @@ import java.util.Map;
  * On 11.06.2019.
  */
 public class AquaObj {
-    private boolean isEatable;
-    private boolean isMoving;
-    private boolean isGrowing;
+
     private Map<AquaObj,AquaCell> inside;
 
-    public void Move(){}
-    public void Growing(){}
+
+    public  void Growing(){}
     public AquaCell getCell(){return new AquaCell();}
     public AquaObj getObj(){return  new AquaObj();}
 
--- src/Grass.java
+++ src/Grass.java
@@ -1,6 +1,19 @@
+import java.util.Random;
+
 /**
  * Created by Лизаза
  * On 11.06.2019.
  */
-public class Grass {
+public class Grass extends AquaObj{
+    private final static int maxSize = 15;
+    private int Size;
+
+    public Grass() {
+        Size = new Random().nextInt(15);
+    }
+
+    @Override
+    public void Growing() {
+        this.Size++;
+    }
 }
