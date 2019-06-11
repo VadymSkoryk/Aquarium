71fda7eb5e3b7a64a755f74e7b9d37d13aa03d84
Vadym Skoryk <yopt322@gmail.com>
2019-06-11 13:46:43 +0300

    Create classes Predator and Herb fishes.

---
+++ src/HerbFish.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Лизаза
+ * On 11.06.2019.
+ */
+public class HerbFish extends Fish {
+}
---
+++ src/PredatorFish.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Лизаза
+ * On 11.06.2019.
+ */
+public class PredatorFish extends Fish {
+    public PredatorFish() {
+    }
+}
